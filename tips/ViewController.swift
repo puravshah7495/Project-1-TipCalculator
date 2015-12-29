61595a65370e63eeb10e1a156fe6c9e69a6283af
Purav Shah <puravshah@Puravs-Mac-Pro.local>
2015-12-29 18:15:21 -0500

    Add UI animations

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -14,28 +14,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var bottomView: UIView!
     
     let defaults = NSUserDefaults.standardUserDefaults()
+    var previousInput:NSString = ""
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-        var defaultIndex = defaults.integerForKey("default_index")
-        var previousBill = defaults.stringForKey("previous_bill")
-        var billTimeout = defaults.objectForKey("bill_timeout")
+        bottomView.alpha = 0
+        billField.frame.origin.y = billField.frame.height * 1.8
         
-        if (previousBill != nil && billTimeout != nil) {
-            var timeout:NSDate = billTimeout as! NSDate
-            var dateComparison:NSComparisonResult = NSDate().compare(timeout)
+        let defaultIndex = defaults.integerForKey("default_index")
+        let previousBill = defaults.stringForKey("previous_bill")
+        let billTimeout = defaults.objectForKey("bill_timeout")
+        
+        if (previousBill != nil && previousBill! != "" && billTimeout != nil) {
+            let timeout:NSDate = billTimeout as! NSDate
+            let dateComparison:NSComparisonResult = NSDate().compare(timeout)
             if (dateComparison == NSComparisonResult.OrderedAscending) {
+                print(previousBill)
                 billField.text = previousBill
+                bottomView.alpha = 1
+                billField.frame.origin.y -= billField.frame.height
             } else {
-                defaults.setNilValueForKey("previous_bill")
+                defaults.setObject(nil, forKey: "previous_bill")
+                defaults.synchronize()
             }
         }
         
         tipControl.selectedSegmentIndex = defaultIndex
-        
+
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -60,7 +69,7 @@ class ViewController: UIViewController {
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
-        var input = billField.text!
+        let input = billField.text!
         defaults.setObject(input, forKey: "previous_bill")
         defaults.setObject(NSDate(timeIntervalSinceNow: 600), forKey: "bill_timeout")
         defaults.synchronize()
@@ -68,19 +77,31 @@ class ViewController: UIViewController {
     
     @IBAction func onEditingChanged(sender: AnyObject) {
         var tipPercentages = [0.18,0.2,0.22]
-        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let bill = billField.text!
         
-        var billAmount = NSString(string: billField.text!).doubleValue
-        var tip = billAmount * tipPercentage
-        var total = billAmount + tip
+        let billAmount = NSString(string: bill).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
         
+        if (bill != "" && previousInput == "") {
+            UIView.animateWithDuration(0.2, animations: {
+                self.bottomView.alpha = 1
+                self.billField.frame.origin.y -= self.billField.frame.height
+            })
+        }
+        if (bill == "" && previousInput != "") {
+            UIView.animateWithDuration(0.2, animations: {
+                self.bottomView.alpha = 0
+                self.billField.frame.origin.y += self.billField.frame.height
+            })
+        }
+        
+        previousInput = bill
     }
     
-    /*@IBAction func onTap(sender: AnyObject) {
-        view.endEditing(true)
-    }*/
 }
 
